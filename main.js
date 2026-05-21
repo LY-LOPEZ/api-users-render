13f5683f06a767cda35a58e0a6cacb729dd961fa
LY-LOPEZ <lopez.yapu.marco@usfx.bo>
2026-05-21 15:56:21 -0400

    subiendo api rest node

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware for parsing JSON
 app.use(express.json());
-
+app.get('/', (req, res) => {
+    res.send('API REST de usuarios funcionando correctamente');
+});
 // User class
 class User {
     constructor(id, name, username, email, password, updatedAt, image, rol) {
